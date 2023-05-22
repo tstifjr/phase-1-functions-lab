01bc0e38f06247af2c9b044d32015a6465fb6c36
TJ Stifter <tjstifter@gmail.com>
2023-05-22 12:53:16 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance){
+    const Hq = 42;
+    return Math.abs(distance - Hq);
+}
+
+function distanceFromHqInFeet(distance){
+    const Hq = 42;
+    const blocksAway = Math.abs(distance - Hq);
+    return blocksAway * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264;
+    
+}
+
+function calculatesFarePrice(start, destination){
+    const distanceInFeet = Math.abs(start - destination) * 264;
+    if (destination-start === 1){
+        return 0;
+    }
+
+   if (distanceInFeet > 2500){
+    return "cannot travel that far";
+   }
+   else if (distanceInFeet > 2000)
+   {
+    return 25;
+   }
+   else if (distanceInFeet <= 2000){
+    return .02 * (distanceInFeet-400);
+   }
+
+}
+
